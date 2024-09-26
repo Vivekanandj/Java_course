90b1e1f780af3a0063d5991632c9ea1be17e6186
Vivekanandj <121560515+Vivekanandj@users.noreply.github.com>
2024-09-26 12:18:47 +0530

    Upto date

---
+++ src/cwh_34_recursion.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class cwh_34_recursion {
+    static int factorial(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        return n*factorial(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(factorial(n));
+    }
+}
