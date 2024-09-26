96230b3e98454f2b0e77e348854e11ad69f2a5de
Vivekanandj <121560515+Vivekanandj@users.noreply.github.com>
2024-09-26 14:39:27 +0530

    Upto date

--- src/cwh_34_recursion.java
+++ src/cwh_34_recursion.java
@@ -7,9 +7,22 @@ public class cwh_34_recursion {
         }
         return n*factorial(n-1);
     }
+    static int factorial_iterative(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        else {
+            int factorial = 1;
+            for(int i=1;i<=n;i++){
+                factorial = factorial*i;
+            }
+            return factorial;
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         System.out.println(factorial(n));
+        System.out.println(factorial_iterative(n));
     }
 }
---
+++ src/cwh_fibonacci.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class cwh_fibonacci {
+    public static void fib(int n){
+        int f = 0;
+        int f1 = 1;
+        System.out.print("Fibonacci Series of " + n + " numbers: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(f + " ");
+            int f3= f1+f;
+            f = f1;
+            f1 = f3;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        fib(n);
+        System.out.println(n);
+    }
+}
